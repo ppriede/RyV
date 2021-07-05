999440060f3ac2d89141e6067372d3d2f18fd0b7
Patricio Priede <17771721+ppriede@users.noreply.github.com>
2021-07-04 22:08:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = MultiApp()
 
 # Add all your application here
 app.add_app("Divergencia geométrica simple", divergencia_geometrica_v1.app)
-#app.add_app("Home", home.app)
-#app.add_app("Data Stats", data_stats.app)
+app.add_app("Home", home.app)
+app.add_app("Data Stats", data_stats.app)
 
 # The main app
 app.run()
